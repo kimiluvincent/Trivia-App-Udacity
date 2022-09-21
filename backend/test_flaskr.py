4b12f2bbba67b35ef7be1a81c2f89b12f93fb146
kimiluvincent <kimiluvincent@gmail.com>
2022-09-21 12:12:33 +0300

    400 exception abort commit

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -34,6 +34,8 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    de
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
